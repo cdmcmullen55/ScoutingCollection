f6445e8f395e9aa82b9a9af62ed22655246848ae
Cole McMullen <cdgator55@gmail.com>
2017-09-27 21:29:37 -0400

    Add project files.

---
+++ ScoutingCollection/ScoutingCollection/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScoutingCollection
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Team.Text = null;
+            Match.Text = null;
+            PitScout.IsEnabled = false;
+            MatchScout.IsEnabled = false;
+        }
+
+        async void PitScout_Clicked(object sender, EventArgs e) => await Navigation.PushModalAsync(new PitScouting
+        {
+            BindingContext = Team
+        });
+
+        private void MatchScout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Team_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = (Entry)sender;
+            Int32 result;
+            PitScout.IsEnabled = Int32.TryParse(Team.Text, out result);
+        }
+
+        private void Match_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = (Entry)sender;
+            MatchScout.IsEnabled = Int32.TryParse(Match.Text, out int result) &&
+                                   Int32.TryParse(Team.Text, out result);
+        }
+    }
+}
---
+++ ScoutingCollection/ScoutingCollection/PitScoutingGeneral.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScoutingCollection
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PitScoutingGeneral : ContentPage
+    {
+        public PitScoutingGeneral()
+        {
+            InitializeComponent();
+        }
+
+        private void FPS_known_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void UsesFuel_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void UsesGears_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Shifts_gears_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Vision_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Active_gear_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+
+        private void Ground_gear_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
+}
