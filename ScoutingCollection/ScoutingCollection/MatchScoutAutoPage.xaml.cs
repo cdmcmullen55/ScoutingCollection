34aecd008f62a3cd1c3a358d488888435cc8d9d2
Cole McMullen <cdgator55@gmail.com>
2017-11-12 13:00:57 -0500

    Began PostMatchScoutPage

--- ScoutingCollection/ScoutingCollection/MatchScoutAutoPage.xaml.cs
+++ ScoutingCollection/ScoutingCollection/MatchScoutAutoPage.xaml.cs
@@ -24,6 +24,7 @@ namespace ScoutingCollection
             {
                 ((BindingContext as MainViewModel).currentReport as MatchVM).Tele = false;
                 ((BindingContext as MainViewModel).currentReport as MatchVM).setButtonProperties();
+                baseline.IsEnabled = true;
             }
             else
                 await Navigation.PopModalAsync();
@@ -41,10 +42,26 @@ namespace ScoutingCollection
             ((BindingContext as MainViewModel).currentReport as MatchVM).RefreshButtons();
         }
 
-        private void continue_Clicked(object sender, EventArgs e)
+        async void continue_Clicked(object sender, EventArgs e)
         {
-            ((BindingContext as MainViewModel).currentReport as MatchVM).Tele = true;
-            ((BindingContext as MainViewModel).currentReport as MatchVM).setButtonProperties();
+            if (((BindingContext as MainViewModel).currentReport as MatchVM).Tele)
+            {
+                var finish = await DisplayAlert("Finish?", "You will not be able to make changes after continuing.", "Finish", 
+                    "Cancel");
+                if (finish)
+                {
+                    var postmatchpage = new PostMatchScoutPage((BindingContext as MainViewModel), this);
+                    await Navigation.PushModalAsync(postmatchpage);
+                }
+                else
+                    return;
+            }
+            else
+            {
+                ((BindingContext as MainViewModel).currentReport as MatchVM).Tele = true;
+                ((BindingContext as MainViewModel).currentReport as MatchVM).setButtonProperties();
+                baseline.IsEnabled = false;
+            }
         }
     }
 }
---
+++ ScoutingCollection/ScoutingCollection/PostMatchScoutPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScoutingCollection
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PostMatchScoutPage : ContentPage
+	{
+        public MatchScoutAutoPage page;
+
+        public PostMatchScoutPage ()
+		{
+			InitializeComponent ();
+		}
+
+        public PostMatchScoutPage(MainViewModel mvm, MatchScoutAutoPage prev_page)
+        {
+            BindingContext = mvm;
+            page = prev_page;
+            InitializeComponent();
+        }
+
+        async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PopModalAsync(false);
+            await Application.Current.MainPage.Navigation.PopModalAsync();
+        }
+    }
+}
